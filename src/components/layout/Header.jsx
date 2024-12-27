56cc93837e5b1860d96702b35d1bcc475decbee8
Shane <shanegoodson101@gmail.com>
2024-12-27 09:57:36 +0200

    title fix

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -6,15 +6,15 @@ const Header = () => {
 
   return (
     <header className="bg-surface0 border-b border-surface1 p-4 sticky top-0 z-10">
-      <div className="flex items-center">
+      <div className="flex items-center relative">
         <button
           onClick={toggleSidebar}
-          className="p-2 hover:bg-surface1 rounded text-overlay2 self-start"
+          className="p-2 hover:bg-surface1 rounded text-overlay2"
         >
           <Menu className="h-5 w-5" />
         </button>
         
-        <div className="text-subtext0 self-center">
+        <div className="absolute left-1/2 -translate-x-1/2 text-subtext0">
           A practical guide to render-driven architecture
         </div>
       </div>
